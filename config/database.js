9243aa2bbcdf36188d9044908112eb41a9ffe19b
Frekki <sobolrip1@gmail.com>
2017-08-15 16:30:44 +0300

    Linked DB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/teamview',
+  database: 'mongodb://localhost:27017/teamview' || 'mongodb://freky:4512923@ds145183.mlab.com:45183/teamview-app',
   secret: 'yoursecret'
 }
